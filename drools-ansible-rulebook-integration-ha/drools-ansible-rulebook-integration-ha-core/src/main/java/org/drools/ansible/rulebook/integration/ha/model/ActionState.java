0b8b94c8a1618e714c476ecc86173386bbaf719e
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2025-09-04 11:47:55 +0900

    update ActionState model. WIP still need to fix tests
    (cherry picked from commit 1b7360c2076ce3c6c97dbc96dc87e4ab1960eedf)

--- drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/model/ActionState.java
+++ drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/model/ActionState.java
@@ -1,106 +1,24 @@
 package org.drools.ansible.rulebook.integration.ha.model;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 /**
- * Represents the state of actions for a matching event
+ * Represents a single action state for a matching event
  */
 public class ActionState {
     
-    private String meUuid;
-    private String rulesetName;
-    private String ruleName;
-    private List<Action> actions = new ArrayList<>();
-    private int version;
-    private String updatedAt;
-    
-    public static class Action {
-        private String name;
-        private int index;
-        private ActionStatus status;
-        private String startedAt;
-        private String endedAt;
-        private String referenceId;
-        private String referenceUrl;
-        private Map<String, Object> customData;
-        
-        public enum ActionStatus {
-            RUNNING,
-            SUCCESS,
-            FAILED
-        }
-        
-        public String getName() {
-            return name;
-        }
-        
-        public void setName(String name) {
-            this.name = name;
-        }
-        
-        public int getIndex() {
-            return index;
-        }
-        
-        public void setIndex(int index) {
-            this.index = index;
-        }
-        
-        public ActionStatus getStatus() {
-            return status;
-        }
-        
-        public void setStatus(ActionStatus status) {
-            this.status = status;
-        }
-        
-        public String getStartedAt() {
-            return startedAt;
-        }
-        
-        public void setStartedAt(String startedAt) {
-            this.startedAt = startedAt;
-        }
-        
-        public String getEndedAt() {
-            return endedAt;
-        }
-        
-        public void setEndedAt(String endedAt) {
-            this.endedAt = endedAt;
-        }
-        
-        public String getReferenceId() {
-            return referenceId;
-        }
-        
-        public void setReferenceId(String referenceId) {
-            this.referenceId = referenceId;
-        }
-        
-        public String getReferenceUrl() {
-            return referenceUrl;
-        }
-        
-        public void setReferenceUrl(String referenceUrl) {
-            this.referenceUrl = referenceUrl;
-        }
-        
-        public Map<String, Object> getCustomData() {
-            return customData;
-        }
-        
-        public void setCustomData(Map<String, Object> customData) {
-            this.customData = customData;
-        }
-    }
+    private String id;          // UUID primary key
+    private String meUuid;       // Foreign key to MatchingEvent
+    private int index;           // Action index within the matching event
+    private String actionData;   // JSON blob containing action details
     
     public ActionState() {
-        this.updatedAt = Instant.now().toString();
-        this.version = 1;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
     }
     
     public String getMeUuid() {
@@ -111,43 +29,19 @@ public class ActionState {
         this.meUuid = meUuid;
     }
     
-    public String getRulesetName() {
-        return rulesetName;
+    public int getIndex() {
+        return index;
     }
     
-    public void setRulesetName(String rulesetName) {
-        this.rulesetName = rulesetName;
+    public void setIndex(int index) {
+        this.index = index;
     }
     
-    public String getRuleName() {
-        return ruleName;
+    public String getActionData() {
+        return actionData;
     }
     
-    public void setRuleName(String ruleName) {
-        this.ruleName = ruleName;
-    }
-    
-    public List<Action> getActions() {
-        return actions;
-    }
-    
-    public void setActions(List<Action> actions) {
-        this.actions = actions;
-    }
-    
-    public int getVersion() {
-        return version;
-    }
-    
-    public void setVersion(int version) {
-        this.version = version;
-    }
-    
-    public String getUpdatedAt() {
-        return updatedAt;
-    }
-    
-    public void setUpdatedAt(String updatedAt) {
-        this.updatedAt = updatedAt;
+    public void setActionData(String actionData) {
+        this.actionData = actionData;
     }
 }
