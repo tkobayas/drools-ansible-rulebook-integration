33bc6940a2679091374b0237417117455a3564db
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2025-10-08 18:18:44 +0900

    assertFact: enhance HASessionContext

--- drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/api/HASessionContext.java
+++ drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/api/HASessionContext.java
@@ -1,33 +1,102 @@
 package org.drools.ansible.rulebook.integration.ha.api;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.drools.ansible.rulebook.integration.ha.model.EventRecord;
 
 public class HASessionContext {
 
-    // TODO: consider thread-safety. processEvent is called by singele client. But AutomaticPsuedoClock is called by another thread
+    // TODO: consider thread-safety. processEvent is called by single client. But AutomaticPseudoClock can run on another thread
 
-    // TODO: Consider performance. For now, LinkedHashMap because we need Map lookup and order of insertion
-    private LinkedHashMap<String, EventRecord> eventUuidsInMemory = new LinkedHashMap<>();
+    private final LinkedHashMap<String, EventRecord> processedRecords = new LinkedHashMap<>();
+    private final Map<Long, String> factHandleIndex = new HashMap<>();
+    private PendingRecord pendingRecord;
+    private String currentIdentifier;
 
-    private String currentEventUuid;
+    public LinkedHashMap<String, EventRecord> getProcessedRecords() {
+        return processedRecords;
+    }
 
     public LinkedHashMap<String, EventRecord> getEventUuidsInMemory() {
-        return eventUuidsInMemory;
+        return processedRecords;
     }
 
     public void addEventUuidInMemory(String uuid, EventRecord eventRecord) {
-        // TODO: consider MAX_EVENTS? but we already have memory check (MemoryMonitorUtil)
-        eventUuidsInMemory.put(uuid, eventRecord);
-        currentEventUuid = uuid;
+        addRecord(uuid, eventRecord, null);
+    }
+
+    public void addRecord(String identifier, EventRecord record, Long factHandleId) {
+        if (identifier == null || record == null) {
+            return;
+        }
+
+        processedRecords.put(identifier, record);
+        if (factHandleId != null) {
+            factHandleIndex.put(factHandleId, identifier);
+        }
+        currentIdentifier = identifier;
     }
 
     public void removeEventUuidInMemory(String uuid) {
-        eventUuidsInMemory.remove(uuid);
+        removeRecord(uuid);
+    }
+
+    public void removeRecord(String identifier) {
+        if (identifier == null) {
+            return;
+        }
+        processedRecords.remove(identifier);
+    }
+
+    public void removeRecordByFactHandle(long factHandleId) {
+        String identifier = factHandleIndex.remove(factHandleId);
+        if (identifier != null) {
+            processedRecords.remove(identifier);
+        }
+    }
+
+    public String getCurrentIdentifier() {
+        return currentIdentifier;
     }
 
     public String getCurrentEventUuid() {
-        return currentEventUuid;
+        return currentIdentifier;
+    }
+
+    public void preparePendingRecord(String identifier, String json, EventRecord.RecordType type) {
+        pendingRecord = new PendingRecord(identifier, json, type);
+        currentIdentifier = identifier;
+    }
+
+    public PendingRecord consumePendingRecord() {
+        PendingRecord record = pendingRecord;
+        pendingRecord = null;
+        return record;
+    }
+
+    public static final class PendingRecord {
+        private final String identifier;
+        private final String json;
+        private final EventRecord.RecordType type;
+
+        PendingRecord(String identifier, String json, EventRecord.RecordType type) {
+            this.identifier = identifier;
+            this.json = json;
+            this.type = type;
+        }
+
+        public String getIdentifier() {
+            return identifier;
+        }
+
+        public String getJson() {
+            return json;
+        }
+
+        public EventRecord.RecordType getType() {
+            return type;
+        }
     }
 }
