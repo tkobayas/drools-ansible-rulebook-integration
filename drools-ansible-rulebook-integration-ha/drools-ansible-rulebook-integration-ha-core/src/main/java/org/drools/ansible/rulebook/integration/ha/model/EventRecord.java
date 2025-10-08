6789d003496dcbb981f6596870ef6cbe7f89c619
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2025-10-08 17:51:04 +0900

    assertFact: enhance EventRecord

--- drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/model/EventRecord.java
+++ drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/model/EventRecord.java
@@ -2,11 +2,18 @@ package org.drools.ansible.rulebook.integration.ha.model;
 
 public class EventRecord {
 
+    public enum RecordType {
+        EVENT,
+        FACT
+    }
+
     private String eventUuid; // TODO: consider if we exclude this field for persistece for performance
 
     private String eventJson;
 
     private long insertedAt;
+
+    private RecordType recordType = RecordType.EVENT;
 
     // TODO: Probably need to have ID and track the associated event to handle deletions
 
@@ -19,6 +26,11 @@ public class EventRecord {
         this.insertedAt = insertedAt;
     }
 
+    public EventRecord(String eventUuid, String eventJson, long insertedAt, RecordType recordType) {
+        this(eventUuid, eventJson, insertedAt);
+        this.recordType = recordType;
+    }
+
     public String getEventUuid() {
         return eventUuid;
     }
@@ -29,5 +41,13 @@ public class EventRecord {
 
     public long getInsertedAt() {
         return insertedAt;
+    }
+
+    public RecordType getRecordType() {
+        return recordType;
+    }
+
+    public void setRecordType(RecordType recordType) {
+        this.recordType = recordType;
     }
 }
