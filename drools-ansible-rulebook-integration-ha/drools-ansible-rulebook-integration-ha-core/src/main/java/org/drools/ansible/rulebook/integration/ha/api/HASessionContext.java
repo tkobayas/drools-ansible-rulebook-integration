ae71423fa038234f34edeae5eec4f7af3c6e0756
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2025-10-22 16:28:03 +0900

    renamed to trackRecords. removed duplicated methods

--- drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/api/HASessionContext.java
+++ drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/api/HASessionContext.java
@@ -12,53 +12,40 @@ public class HASessionContext {
 
     private static final Logger logger = LoggerFactory.getLogger(HASessionContext.class);
 
-    // TODO: consider thread-safety. processEvent is called by single client. But AutomaticPseudoClock can run on another thread
+    // Track events/facts existing in the session. Eventually persisted as SessionState.partialEvents
+    private final LinkedHashMap<String, EventRecord> trackedRecords = new LinkedHashMap<>();
 
-    private final LinkedHashMap<String, EventRecord> processedRecords = new LinkedHashMap<>();
     private final Map<Long, String> factHandleIndex = new HashMap<>();
     private PendingRecord pendingRecord;
     private String currentIdentifier;
 
-    public LinkedHashMap<String, EventRecord> getProcessedRecords() {
-        return processedRecords;
+    public LinkedHashMap<String, EventRecord> getTrackedRecords() {
+        return trackedRecords;
     }
 
-    public LinkedHashMap<String, EventRecord> getEventUuidsInMemory() {
-        return processedRecords;
-    }
-
-    // TODO: consider unifying method names
-    public void addEventUuidInMemory(String uuid, EventRecord eventRecord) {
-        addRecord(uuid, eventRecord, null);
-    }
-
-    public void addRecord(String identifier, EventRecord record, Long factHandleId) {
+    public void addTrackedRecord(String identifier, EventRecord record, Long factHandleId) {
         if (identifier == null || record == null) {
             return;
         }
 
-        processedRecords.put(identifier, record);
+        trackedRecords.put(identifier, record);
         if (factHandleId != null) {
             factHandleIndex.put(factHandleId, identifier);
         }
         currentIdentifier = identifier;
     }
 
-    public void removeEventUuidInMemory(String uuid) {
-        removeRecord(uuid);
-    }
-
-    public void removeRecord(String identifier) {
+    public void removeTrackedRecord(String identifier) {
         if (identifier == null) {
             return;
         }
-        processedRecords.remove(identifier);
+        trackedRecords.remove(identifier);
     }
 
-    public void removeRecordByFactHandle(long factHandleId) {
+    public void removeTrackedRecordByFactHandle(long factHandleId) {
         String identifier = factHandleIndex.remove(factHandleId);
         if (identifier != null) {
-            processedRecords.remove(identifier);
+            trackedRecords.remove(identifier);
         }
     }
 
@@ -69,10 +56,10 @@ public class HASessionContext {
      * @param factHandleId The fact handle ID of the object being updated
      * @param updatedJson The new JSON representation of the object
      */
-    public void updateRecordByFactHandle(long factHandleId, String updatedJson) {
+    public void updateTrackedRecordByFactHandle(long factHandleId, String updatedJson) {
         String identifier = factHandleIndex.get(factHandleId);
         if (identifier != null) {
-            EventRecord record = processedRecords.get(identifier);
+            EventRecord record = trackedRecords.get(identifier);
             if (record != null) {
                 record.setEventJson(updatedJson);
                 logger.debug("Updated EventRecord for identifier: {}, factHandleId: {}", identifier, factHandleId);
@@ -85,10 +72,6 @@ public class HASessionContext {
     }
 
     public String getCurrentIdentifier() {
-        return currentIdentifier;
-    }
-
-    public String getCurrentEventUuid() {
         return currentIdentifier;
     }
 
