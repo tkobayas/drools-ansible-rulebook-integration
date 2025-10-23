b25cb845a6908f71b46d1a535e2e42bbbe629bf0
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2025-10-23 18:53:46 +0900

    align containerLookupId use for HARulesExecutorSession
    (cherry picked from commit 691424bbece4bf204706a747b7b6ba463189d719)

--- drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/api/HARulesExecutorSession.java
+++ drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-core/src/main/java/org/drools/ansible/rulebook/integration/ha/api/HARulesExecutorSession.java
@@ -3,9 +3,13 @@ package org.drools.ansible.rulebook.integration.ha.api;
 import org.drools.ansible.rulebook.integration.api.domain.RulesSet;
 import org.drools.ansible.rulebook.integration.api.rulesengine.RulesExecutionController;
 import org.drools.ansible.rulebook.integration.api.rulesengine.RulesExecutorSession;
+import org.drools.ansible.rulebook.integration.api.rulesengine.SessionStats;
 import org.kie.api.runtime.KieSession;
 
 public class HARulesExecutorSession extends RulesExecutorSession {
+
+    // External session ID for HA mode - may differ from internal id after recovery
+    private Long externalSessionId = null;
 
     private final HASessionContext haSessionContext;
     private final HARuleRuntimeEventListener eventListener;
@@ -25,5 +29,19 @@ public class HARulesExecutorSession extends RulesExecutorSession {
 
     public HASessionContext getHaSessionContext() {
         return haSessionContext;
+    }
+
+    /**
+     * Sets external session ID for HA mode.
+     * This ID is what external clients (Python) use and should remain consistent across recovery.
+     */
+    public void setExternalSessionId(Long externalSessionId) {
+        this.externalSessionId = externalSessionId;
+    }
+
+    @Override
+    public long getId() {
+        // Return external session ID if set, otherwise internal ID
+        return externalSessionId != null ? externalSessionId : super.getId();
     }
 }
