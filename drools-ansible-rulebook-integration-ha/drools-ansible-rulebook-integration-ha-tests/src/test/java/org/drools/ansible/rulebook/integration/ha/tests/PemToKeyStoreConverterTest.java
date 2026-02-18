4309cbbce8ec86cf60383a0a1fbbfddf206194be
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2026-02-18 18:14:45 +0900

    test refactoring
    (cherry picked from commit 90f3ac61eae32130a50eed565ee985c8394ddaf0)

--- drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-tests/src/test/java/org/drools/ansible/rulebook/integration/ha/tests/PemToKeyStoreConverterTest.java
+++ drools-ansible-rulebook-integration-ha/drools-ansible-rulebook-integration-ha-tests/src/test/java/org/drools/ansible/rulebook/integration/ha/tests/PemToKeyStoreConverterTest.java
@@ -1,22 +1,13 @@
 package org.drools.ansible.rulebook.integration.ha.tests;
 
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.PrivateKey;
-import java.security.SecureRandom;
-import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.util.Enumeration;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 import org.drools.ansible.rulebook.integration.ha.postgres.PemToKeyStoreConverter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -26,14 +17,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for PEM-to-PKCS#12 key conversion.
  * <p>
- * These tests validate:
- * <ol>
- *   <li>Certificate generation produces valid PEM files</li>
- *   <li>PemToKeyStoreConverter can read traditional encrypted PEM keys</li>
- *   <li>The resulting PKCS#12 keystore contains the correct key and certificate</li>
- *   <li>Unencrypted PEM keys can also be converted</li>
- * </ol>
- * <p>
  * For SSL integration tests against a real PostgreSQL container, see {@link PostgresSSLJdbcTest}.
  */
 class PemToKeyStoreConverterTest {
@@ -41,47 +24,7 @@ class PemToKeyStoreConverterTest {
     @TempDir
     Path tempDir;
 
-    static {
-        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
-            Security.addProvider(new BouncyCastleProvider());
-        }
-    }
-
-    @Test
-    void testCertificateGenerationProducesValidPemFiles() throws Exception {
-        SSLTestCertificateGenerator.CertBundle bundle = SSLTestCertificateGenerator.generate(tempDir.resolve("certs"));
-
-        assertThat(bundle.caCert()).exists();
-        assertThat(bundle.serverKey()).exists();
-        assertThat(bundle.serverCert()).exists();
-        assertThat(bundle.clientKey()).exists();
-        assertThat(bundle.clientCert()).exists();
-
-        // Verify PEM headers
-        String caCertContent = Files.readString(bundle.caCert());
-        assertThat(caCertContent).contains("-----BEGIN CERTIFICATE-----");
-
-        String clientKeyContent = Files.readString(bundle.clientKey());
-        // Traditional encrypted PEM has "BEGIN RSA PRIVATE KEY" with Proc-Type/DEK-Info
-        assertThat(clientKeyContent).contains("-----BEGIN RSA PRIVATE KEY-----");
-        assertThat(clientKeyContent).contains("Proc-Type: 4,ENCRYPTED");
-        assertThat(clientKeyContent).contains("DEK-Info:");
-
-        String clientCertContent = Files.readString(bundle.clientCert());
-        assertThat(clientCertContent).contains("-----BEGIN CERTIFICATE-----");
-    }
-
-    @Test
-    void testPemParserRecognizesEncryptedKey() throws Exception {
-        SSLTestCertificateGenerator.CertBundle bundle = SSLTestCertificateGenerator.generate(tempDir.resolve("certs"));
-
-        try (PEMParser parser = new PEMParser(new FileReader(bundle.clientKey().toFile()))) {
-            Object obj = parser.readObject();
-            assertThat(obj).isNotNull();
-            assertThat(obj.getClass().getSimpleName()).isEqualTo("PEMEncryptedKeyPair");
-        }
-    }
-
+    // Key format: Traditional OpenSSL PEM encryption (Proc-Type / DEK-Info)
     @Test
     void testConvertTraditionalEncryptedPemToP12() throws Exception {
         SSLTestCertificateGenerator.CertBundle bundle = SSLTestCertificateGenerator.generate(tempDir.resolve("certs"));
@@ -119,57 +62,7 @@ class PemToKeyStoreConverterTest {
         assertThat(p12Path).doesNotExist();
     }
 
-    @Test
-    void testIsPemEncrypted() throws Exception {
-        SSLTestCertificateGenerator.CertBundle bundle = SSLTestCertificateGenerator.generate(tempDir.resolve("certs"));
-
-        // Encrypted PEM key should be detected as encrypted
-        assertThat(PemToKeyStoreConverter.isPemEncrypted(bundle.clientKey().toString())).isTrue();
-
-        // Unencrypted PEM key should be detected as not encrypted
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(2048, new SecureRandom());
-        KeyPair keyPair = keyGen.generateKeyPair();
-
-        Path unencryptedKeyPath = tempDir.resolve("unencrypted.pem");
-        try (JcaPEMWriter writer = new JcaPEMWriter(new FileWriter(unencryptedKeyPath.toFile()))) {
-            writer.writeObject(keyPair.getPrivate());
-        }
-
-        assertThat(PemToKeyStoreConverter.isPemEncrypted(unencryptedKeyPath.toString())).isFalse();
-    }
-
-    @Test
-    void testConvertUnencryptedPemToP12() throws Exception {
-        SSLTestCertificateGenerator.CertBundle bundle = SSLTestCertificateGenerator.generate(tempDir.resolve("certs"));
-
-        // Write an unencrypted version of a key
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(2048, new SecureRandom());
-        KeyPair keyPair = keyGen.generateKeyPair();
-
-        Path unencryptedKeyPath = tempDir.resolve("unencrypted.key");
-        try (JcaPEMWriter writer = new JcaPEMWriter(new FileWriter(unencryptedKeyPath.toFile()))) {
-            writer.writeObject(keyPair.getPrivate());
-        }
-
-        // Use a dummy passphrase (for the P12 keystore password)
-        Path p12Path = PemToKeyStoreConverter.convertPemToP12(
-                unencryptedKeyPath.toString(),
-                bundle.clientCert().toString(),
-                "dummypass".toCharArray());
-
-        assertThat(p12Path).exists();
-
-        KeyStore ks = KeyStore.getInstance("PKCS12");
-        try (var is = Files.newInputStream(p12Path)) {
-            ks.load(is, "dummypass".toCharArray());
-        }
-        assertThat(ks.size()).isEqualTo(1);
-
-        PemToKeyStoreConverter.cleanup(p12Path);
-    }
-
+    // Key format: unencrypted PEM PKCS#8
     @Test
     void testConvertUnencryptedPkcs8PemToP12() throws Exception {
         SSLTestCertificateGenerator.CertBundle bundle = SSLTestCertificateGenerator.generate(tempDir.resolve("certs"));
